17ad36f0dbf3b7debe6fc321cad09ea8e98279bd
atkc <ant_tan@outlook.com>
2016-08-30 01:10:01 +0800

    construction of RDF function

--- subfunctions/Radial Distributuion Function/RDF.m
+++ subfunctions/Radial Distributuion Function/RDF.m
@@ -1,4 +1,4 @@
-function [ output_args ] = RDF( points,im,stepR )
+function [ maxR ] = RDF( points,im,nSteps )
 %RDF Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -7,10 +7,17 @@ function [ output_args ] = RDF( points,im,stepR )
 midptX=m/2;
 midptY=n/2;
 
-distXY=((points(1,:)-midptX).^2+(points(2,:)-midptY).^2);
-[minInd,~]=min(distXY);
+distXY=sqrt((points(:,1)-midptX).^2+(points(:,2)-midptY).^2);
+[~,minInd]=min(distXY);
+%%for verification
+% figure;
+% plot(points(:,1),points(:,2),'b.');
+% hold on
+% plot(points(minInd,1),points(minInd,2),'r.')
+maxR=min([points(minInd,1),points(minInd,2),m-points(minInd,1),n-points(minInd,2)]);
+distXYr=sqrt((points(:,1)-points(minInd,1)).^2+(points(:,2)-points(minInd,2)).^2);
 
-
-
+dStep=maxR/nSteps;
+hist(distXYr);
 end
 
